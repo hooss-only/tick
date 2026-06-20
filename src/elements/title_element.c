0ee5ce41cfe8740e33e100a997610ebaef6f6de7
Changmin Jung <hoosshusu@gmail.com>
2026-06-20 17:56:05 +0900

    Add centered text in splash

--- src/elements/title_element.c
+++ src/elements/title_element.c
@@ -8,12 +8,14 @@ static void render(element_t* self);
 
 element_t* get_title_element(void) {
         element_t* e = get_element();
-
+        
+        e->x = GetScreenWidth() / 2.f; e->y = GetScreenHeight() / 2.f;
         e->render = render;
 
         return e;
 }
 
 void render(element_t* self) {
-        DrawText("TICK.", self->x, self->y, 10, WHITE);
+        int width = MeasureText("TICK", 48);
+        DrawText("TICK.", self->x - width/2.f, self->y - 24, 48, WHITE);
 }
