53dab5a0755cb6c8d762babb259b27efaf8eba70
Changmin Jung <hoosshusu@gmail.com>
2026-06-21 08:59:38 +0900

    Add activation

--- src/screens/clock_screen.c
+++ src/screens/clock_screen.c
@@ -4,10 +4,33 @@
 
 #include <raylib.h>
 
+static void update(screen_t* self);
+
+bool activated = false;
+double activated_time = 0;
+
 screen_t* get_clock_screen(void) {
         screen_t* s = get_screen();
+
+        s->update = update;
 
         add_screen_element(s, get_clock_element());
 
         return s;
+}
+
+#define ACTIVATION_DURATION 10
+void update(screen_t* self) {
+        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+                if (!activated) {
+                        activated = true;
+                }
+                activated_time = GetTime();
+        }
+
+        if (activated && GetTime() - activated_time >= ACTIVATION_DURATION) {
+                activated = false;
+        }
+
+        screen_update(self);
 }
