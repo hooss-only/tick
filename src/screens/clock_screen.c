4a7eb5a34358614328f696e518cda5225d152c89
Changmin Jung <hoosshusu@gmail.com>
2026-06-22 23:04:52 +0900

    Make the clock screen able to flip to side pages

--- src/screens/clock_screen.c
+++ src/screens/clock_screen.c
@@ -3,6 +3,7 @@
 #include "elements/clock_element.h"
 
 #include <raylib.h>
+#include <math.h>
 
 static void update(screen_t* self);
 
@@ -25,9 +26,6 @@ bool dragging;
 int initial_mouse_x;
 #define ACTIVATION_DURATION 10
 void update(screen_t* self) {
-        if (!dragging) {
-                self->base.x *= 0.95;
-        }
         if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
                 if (!activated) {
                         activated = true;
@@ -54,4 +52,25 @@ void update(screen_t* self) {
         }
 
         screen_update(self);
+
+        if (!dragging) {
+                if (fabsf(self->base.x) <= 0.01f) {
+                        self->base.x = 0;
+                        return;
+                }
+
+                if (self->base.x > GetScreenWidth() / 2.f) {
+                        self->base.x = GetScreenWidth() - (GetScreenWidth() - self->base.x) * 0.95f;
+                        return;
+                }
+
+                if (self->base.x < GetScreenWidth() / -2.f) {
+                        self->base.x = -1 * GetScreenWidth() + (GetScreenWidth() + self->base.x) * 0.95f;
+                        return;
+                }
+
+                self->base.x *= 0.95;
+                
+        }
+
 }
